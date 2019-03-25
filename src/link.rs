9523f01a2d1d2204a16c123b110345092016dd33
igniscyan <bwclark97@ufl.edu>
2019-03-25 19:43:47 -0400

    Got base file creation finsihed.

---
+++ src/link.rs
@@ -0,0 +1,50 @@
+use std::fs::File;
+use symlink;
+use std::path::Path;
+use std::path::PathBuf;
+use std::fs::DirEntry;
+use std::error::Error;
+
+
+pub fn create_base_sym(file_path: PathBuf)->std::io::Result<()>{
+   for entry in fs::read_dir(file_path)? {
+        if let Ok(entry) = entry { 
+            let display = file_path.display();
+            let mut fileName = entry.file_name();
+            fileName = fileName + ".base"; 
+            let &mut the_file: match File::open(&file_path) { 
+                Err(why) => panic!("couldn't open {}: {}", display,
+                                                           why.description()),
+                Ok(file) => file, 
+            }
+            let mut s = String::new(); 
+
+            match the_file.read_to_string(&mut s) { 
+                Err(why) => panic!("Couldn't read {}: {}", display,
+                                                           why.description()), 
+                Ok(_) => print!("{} contains:\n{}", display, s), 
+            }
+            file_path.pop;
+            file_path.push(fileName); 
+            
+
+            let mut base_file: File = match File::create(&file_path) { 
+                Err(why) => panic!("couldn't create {}: {}",
+                                            display, 
+                                            why.description())
+                Ok(base_file) => base_file,
+            } 
+            
+            match base_file.write_all(s.as_bytes()) {
+                Err(why) => { 
+                    panic!("couldn't write to {}: {}", display, 
+                                                       why.description())
+                }, 
+
+                Ok(_) => println!("Successfully wrote to .base"),
+            }
+        }
+   }
+}
+
+
